f42af53c9600ad09aaf72fe4ca877571ce1327a7
J0hnny1 <googoj23@gmail.com>
2021-12-14 23:39:20 +0100

    added InputProcessor, addd Walking Textures, added interactive Health bar

---
+++ core/src/com/mygdx/game/InputProcessor.java
@@ -0,0 +1,79 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.Input;
+
+public class InputProcessor implements com.badlogic.gdx.InputProcessor {
+    boolean fullscreen = false;
+    //MyGdxGame mydx = new MyGdxGame();
+
+    /**
+     *
+     * @param keycode
+     * @return if action is performed
+     */
+    @Override
+    public boolean keyDown(int keycode) {
+        switch (keycode) {
+            case Input.Keys.F:
+                if (!fullscreen) {
+                Graphics.Monitor currMonitor = Gdx.graphics.getMonitor();
+                Graphics.DisplayMode displayMode = Gdx.graphics.getDisplayMode(currMonitor);
+                fullscreen = true;
+                if (!Gdx.graphics.setFullscreenMode(displayMode)) {
+                    // switching to full-screen mode failed
+                }
+
+            } else {
+                Gdx.graphics.setWindowedMode(1280, 720);
+                fullscreen = false;
+            }
+                break;
+            case Input.Keys.U:
+                //if (mydx.isShow_region()) {
+                //    mydx.isShow_region() = false;
+                //}else mydx.show_region = true;
+                break;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
+    }
+
+
+}
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,9 +1,6 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Graphics;
-import com.badlogic.gdx.Input;
+import com.badlogic.gdx.*;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -12,59 +9,63 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.utils.TimeUtils;
-import com.badlogic.gdx.utils.Timer;
 
 
 public class MyGdxGame extends ApplicationAdapter {
     SpriteBatch batch;
-    Texture img;
     Texture badlogic;
-    Texture playerTexture;
     OrthographicCamera camera;
     Rectangle player;
     Rectangle badlogic1;
     Texture enemyTexture;
-    Rectangle enemy;
     Sound m9;
-    Rectangle wall;
-    Texture wall2;
     Texture testTexture;
     Rectangle test;
-    boolean fullscreen = false;
     public boolean maybe = true;
     Texture healthTexture;
     Rectangle healthIcon;
-    Health health = new Health(4, 420);
+    Health health = new Health(4, 10);
     Texture male17;
-    TextureRegion region;
-    TextureRegion region2;
-    Rectangle sp2;
-    Rectangle sp3;
+    TextureRegion player_walk_left;
+    TextureRegion player_walk_front;
+    TextureRegion player_walk_right;
+    TextureRegion player_walk_back;
+    Rectangle enemy01;
     Sprite sprite;
     TextureRegion[][] regions;
-    int frame;
-    int zeile;
-    //Instant start_time = Instant.now();
-    //Instant stop_time;;
     long start_time = System.currentTimeMillis();
-    long stop_time;
-    long diff;
-    long diff_s;
+    InputProcessor inputProcessor = new InputProcessor();
+    boolean show_player_front = true;
+    boolean show_player_back;
+    boolean show_player_left;
+    boolean show_player_right;
+    Texture enemy09_texture;
+    TextureRegion enemy09_front;
+    int count = 32;
+    Texture redpotion_texture;
+    TextureRegion redpotion;
+    Rectangle redpotion_rectangle;
 
 
     @Override
     public void create() {
         //Texturen
         badlogic = new Texture("badlogic.jpg");
-        playerTexture = new Texture("stardewT.png");
         enemyTexture = new Texture("shit.png");
         testTexture = new Texture("test.png");
         healthTexture = new Texture("health.png");
+        enemy09_texture = new Texture("Enemy 09-1.png");
+        redpotion_texture = new Texture("red potion.png");
+
 
         male17 = new Texture("Male 17-1.png");
-        region = new TextureRegion(male17, 0, 32, 32, 32);
-        region2 = new TextureRegion(male17, 0, 0, 32, 32);
+        player_walk_left = new TextureRegion(male17, 0, 32, 32, 32);
+        player_walk_right = new TextureRegion(male17, 0, 64, 32, 32);
+        player_walk_front = new TextureRegion(male17, 0, 0, 32, 32);
+        player_walk_back = new TextureRegion(male17, 0, 96, 32, 32);
+
+        enemy09_front = new TextureRegion(enemy09_texture, 0, 0, 32, 32);
+        redpotion = new TextureRegion(redpotion_texture, 0, 0, 16, 16);
 
 
         //sound
@@ -76,117 +77,91 @@ public class MyGdxGame extends ApplicationAdapter {
         camera.setToOrtho(false, 1280, 720);
 
         //player
-        player = new Rectangle();
-        player.x = 1280 / 2 - 64 / 2;
-        player.y = 720 / 2;
-        player.width = 64;
-        player.height = 64;
+        player = new Rectangle(1280 / 2 - 32 / 2, 720 / 2, 32, 32);
 
-        //badlogic
-        badlogic1 = new Rectangle(200, 200, 64, 64);
-
-        //enemy
-        enemy = new Rectangle(400, 200, 64, 64);
-
-        //wall
-        wall = new Rectangle(700, 200, 20, 90);
-
-        test = new Rectangle(100, 100, 93, 93);
+        //enemy01
+        enemy01 = new Rectangle(32, 200, 32, 32);
 
         healthIcon = new Rectangle();
 
-        sp2 = new Rectangle(0, 0, 32, 32);
-        sp3 = new Rectangle(96, 0, 96, 96);
         regions = TextureRegion.split(male17, 32, 32);
         sprite = new Sprite(regions[0][0]);
+
+        redpotion_rectangle = new Rectangle(100, 500, 64, 64);
+        // Input Prozessor
+        Gdx.input.setInputProcessor(inputProcessor);
 
 
     }
 
     @Override
     public void render() {
+
+
         ScreenUtils.clear(216, 158, 85, 0);
         camera.update();
         batch.setProjectionMatrix(camera.combined);
         batch.begin(); //Start des Draw Prozess
 
-        //batch.draw(playerTexture, player.x, player.y);
-        batch.draw(playerTexture, player.x, player.y);
-        //batch.draw(badlogic, badlogic1.x, badlogic1.y);
-        batch.draw(enemyTexture, enemy.x, enemy.y);
-        batch.draw(testTexture, test.x, test.y);
-        batch.draw(healthTexture, 3, 400 - 32);
-        batch.draw(healthTexture, 35, 400 - 32);
-        batch.draw(healthTexture, 70, 400 - 32);
-        batch.draw(region, sp2.x, sp2.y, 96, 96);
-        batch.draw(region2, sp3.x, sp3.y, 96, 96);
+
+        //batch.draw(healthTexture, 3, 720 - 32);
+        //batch.draw(healthTexture, 35, 720 - 32);
+        //batch.draw(healthTexture, 70, 720 - 32);
+        batch.draw(enemy09_front, enemy01.x, enemy01.y, 96, 96);
+        batch.draw(redpotion, redpotion_rectangle.x, redpotion_rectangle.y, 64, 64);
 
 
-        //setzt Farbe aller geladenen Texturen
-        //batch.setColor(1,0,0,1);
+        if (show_player_right) batch.draw(player_walk_right, player.x, player.y, 96, 96);
+        if (show_player_left) batch.draw(player_walk_left, player.x, player.y, 96, 96);
+        if (show_player_back) batch.draw(player_walk_back, player.x, player.y, 96, 96);
+        if (show_player_front) batch.draw(player_walk_front, player.x, player.y, 96, 96);
 
+        for (int i = 0; i < health.getHealth(); i++) {
+            batch.draw(healthTexture, i * 32, 720 - 32);
+        }
 
         //Ende Des Draw Prozess
         batch.end();
 
         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            show_player_left = false;
+            show_player_right = true;
+            show_player_back = false;
+            show_player_front = false;
             player.x += 250 * Gdx.graphics.getDeltaTime();
         }
         if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            show_player_right = false;
+            show_player_left = true;
+            show_player_back = false;
+            show_player_front = false;
             player.x -= 250 * Gdx.graphics.getDeltaTime();
         }
         if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            show_player_right = false;
+            show_player_left = false;
+            show_player_front = false;
+            show_player_back = true;
             player.y += 250 * Gdx.graphics.getDeltaTime();
-            //m9.play();
+
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            show_player_right = false;
+            show_player_left = false;
+            show_player_back = false;
+            show_player_front = true;
+            player.y -= 250 * Gdx.graphics.getDeltaTime();
+
         }
         if (Gdx.input.isKeyPressed(Input.Keys.K)) {
-            System.out.println("\nenemy x: " + enemy.x);
-            System.out.println("enemy y: " + enemy.y);
+            System.out.println("\nenemy x: " + enemy01.x);
+            System.out.println("enemy y: " + enemy01.y);
             System.out.println("player x: " + player.x);
             System.out.println("player y: " + player.y);
 
             System.out.println("Hp: " + health.getHealth());
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-            player.y -= 250 * Gdx.graphics.getDeltaTime();
-            //System.out.println(player.x);
-            //if (player.overlaps(badlogic1)) {
-            //    System.out.println("Poggers");
-            //}
-        }
 
-        //Fullscreen Toggle
-        if (Gdx.input.isKeyPressed(Input.Keys.F)) {
-            if (!fullscreen) {
-                Graphics.Monitor currMonitor = Gdx.graphics.getMonitor();
-                Graphics.DisplayMode displayMode = Gdx.graphics.getDisplayMode(currMonitor);
-                fullscreen = true;
-                if (!Gdx.graphics.setFullscreenMode(displayMode)) {
-                    // switching to full-screen mode failed
-                }
-
-            } else {
-                Gdx.graphics.setWindowedMode(1280, 720);
-                fullscreen = false;
-            }
-
-        }
-
-
-        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
-            //stop_time = Instant.now();
-            //System.out.println(Duration.between(start_time,stop_time));
-            //System.out.println(ChronoUnit.SECONDS.between(start_time, stop_time));
-            //stop_time = System.currentTimeMillis();
-            //diff = stop_time - start_time;
-
-
-            System.out.println("Ms start: " + start_time);
-            System.out.println("Ms stop " + stop_time);
-            System.out.println("Differenz " + diff);
-            System.out.println("Diff S " + diff_s);
-
-        }
 
         // Wenn player ausserhalb des Screens im X bereich
         if (player.x < 0) player.x = 0;
@@ -197,22 +172,12 @@ public class MyGdxGame extends ApplicationAdapter {
         if (player.y + 64 > 720) player.y = 720 - 64;
 
 
-        //for (int i = 0; i < enemy.x + enemyTexture.getWidth(); i++){
-        //    System.out.println("plsh help");
-        //}
-        //shit colision
-
-        //if (player.y > enemy.y && enemy.x == player.x ){
-        //    //player.y = enemy.y;
-        //    System.out.println("aÜtjü");
+        // if (player.overlaps(enemy01)) {
+        //   m9.play(0.4f);
+        //   health.decrease(1);
         //}
 
-        if (player.overlaps(enemy)) {
-            m9.play(0.4f);
-            health.decrease(1);
-        }
-
-        if (player.overlaps(sp3)) {
+        if (player.overlaps(enemy01)) {
 
             if (System.currentTimeMillis() - start_time > 3000 || System.currentTimeMillis() - start_time == 0) {
                 start_time = System.currentTimeMillis();
@@ -223,12 +188,28 @@ public class MyGdxGame extends ApplicationAdapter {
 
         }
 
+        if (player.overlaps(redpotion_rectangle)) {
+                health.increase(1);
+                System.out.println("Hp increase");
+
+
+        }
+
 
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        img.dispose();
+        male17.dispose();
+        healthTexture.dispose();
+        testTexture.dispose();
+        enemyTexture.dispose();
+
+    }
+
+    public void drawHealthIcon() {
+        int c = health.getHealth();
+
     }
 }
